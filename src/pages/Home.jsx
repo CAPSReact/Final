302fa1e0bd7b39c880e77c62b216244ff2d6a6aa
bianbbc87 <bian87@dgu.ac.kr>
2024-01-24 02:48:34 +0900

    [ADD] home 기본 페이지 추가

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Welcome from "../components/Welcome";
+import "../styles/style.css";
+
+export default function Home() {
+  return (
+    <div className="wrapper">
+      <Welcome name="정은지" />
+    </div>
+  );
+}
