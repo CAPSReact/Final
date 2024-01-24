07c09ef04c90e52fdbdbecf36857a7e47f79cfaa
bianbbc87 <bian87@dgu.ac.kr>
2024-01-24 18:08:19 +0900

    [ADD] router 경로 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import House from "./pages/views/House";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Signin from "./pages/sign/Signin";
+import Signup from "./pages/sign/Signup";
+import Navigation from "./components/navigation/Navigation";
 
 function App() {
   return (
     <>
-      <House />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 }
